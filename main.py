ccc97fb79ffdb0beebd54ee5563c2c3878bb9b33
unknown <chowavc@gmail.com>
2021-06-11 04:45:56 +0530

    Added scripts

---
+++ main.py
@@ -0,0 +1,35 @@
+import argparse
+
+from utils import *
+
+def main(args):
+
+	## Convert video to frames
+	if args.v2f:
+
+		print('\nConverting video to frames')
+
+		videoToFrames(args.vDir)
+
+	# Convert frames to video
+	if args.v2f:
+
+		print('\nConverting frames to video')
+
+		framesToVideo(args.fDir)
+
+	print('\nmain.py executed successfully.')
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Reading info for main.')
+
+	# Arguments to convert video to frames
+	parser.add_argument('--v2f', action='store_true', help='Flag to convert video to frames.')
+	parser.add_argument('--vDir', action='store', nargs='?', type=str, default='input/example.avi', help='Source video.')
+
+	# Arguments to converts frames to video
+	parser.add_argument('--f2v', action='store_true', help='Flag to convert frames to video.')
+	parser.add_argument('--fDir', action='store', nargs='?', type=str, default='input/example/*.tif', help='Source frames.')
+
+	main()
---
+++ utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+import cv2
+import glob
+
+## Convert video to frames
+def videoToFrames(videoDir):
+	print('Done.')
+
+## Convert frames to video
+def framesToVideo(framesDir):
+	print('Done.')
